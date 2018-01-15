addf008479251584c0fe74b7b618a35b1aa3b91a
user <user@users-MacBook-Pro.local>
2018-01-15 14:37:59 -0800

    second commit, made self the datasource and delegate and had button append to the wordarray. Finished

--- BeastList/BeastList/ViewController.swift
+++ BeastList/BeastList/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var wordArray:[String] = []
+    
+    @IBOutlet var myTextField: UITextField!
+    
+    @IBOutlet var myTableView: UITableView!
+    
+    // Button pressed
+    
+    @IBAction func beastButtonIsPressed(_ sender: UIButton) {
+        
+        if myTextField.text == nil {
+            return
+        }
+        
+        if let newWord = myTextField.text {
+            
+            wordArray.append(newWord)
+            myTableView.reloadData()
+        }
+        else {
+            return
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    }
+    
+    // Protocol and Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wordArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = wordArray[indexPath.row]
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
